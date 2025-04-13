ad7ad0531b1970787e401a98dc245342a9adcdf1
SarraMess1 <Sarra.Messaoudia@esprit.tn>
2025-04-13 19:35:55 +0100

    Interface Ajout Evenement

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,14 @@
-module com.example.rewear {
+module rewear {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.graphics;
     requires java.sql;
 
-
     opens com.example.rewear to javafx.fxml;
+    opens com.example.rewear.Gestionevent to javafx.fxml;
+    opens com.example.rewear.Gestionevent.Controller to javafx.fxml;
+
     exports com.example.rewear;
+    exports com.example.rewear.Gestionevent;
+    exports com.example.rewear.Gestionevent.Controller;
 }
